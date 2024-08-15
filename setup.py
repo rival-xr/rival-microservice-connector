f49b1fc9e08ee2a2cec0281440211b4825224db5
nicolasprevot2 <76103627+nicolasprevot2@users.noreply.github.com>
2024-08-15 13:30:59 +0100

    bump library version, check if it means we re-pull locally

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rival_microservice_connector",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
         "sentry-sdk>=2.13.0",
