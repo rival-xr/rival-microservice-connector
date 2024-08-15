3490723007425e4f372a8ea008ab2370db128f83
nicolasprevot2 <76103627+nicolasprevot2@users.noreply.github.com>
2024-08-15 11:39:05 +0100

    add install require

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ setup(
     name="rival_microservice_connector",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "sentry-sdk>=2.13.0",
+        "pika>=1.3.2"
+    ],
     python_requires='>=3.6',
     setup_requires=[
         "setuptools>=61.0",
