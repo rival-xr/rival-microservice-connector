068412156e2f2462bd30ac4f1691a69dcf6c657a
nicolasprevot2 <76103627+nicolasprevot2@users.noreply.github.com>
2024-08-15 13:16:11 +0100

    RD-384: add logging to library (#1)
    * configure logging
    
    * no argument
    
    * rename

---
+++ rival_microservice_connector/loggingHelper.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+
+class _OutFilter(logging.Filter):
+    def filter(self, rec):
+        return rec.levelno in (logging.DEBUG, logging.INFO)
+
+def configure_logging():
+  outHandler = logging.StreamHandler(sys.stdout)
+  outHandler.addFilter(_OutFilter())
+
+  errHandler = logging.StreamHandler()
+  errHandler.setLevel(logging.WARNING)
+
+  logging.basicConfig(
+      level=logging.INFO,
+      format='%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s',
+      handlers=[outHandler, errHandler]
+)
