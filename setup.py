dbff6d6d5cfca4f3eec35de8df61c5e44a982bad
Nicolas Prevot <nicolas.prevot@rival-xr.com>
2024-08-22 16:16:43 +0100

    add missing dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     packages=find_packages(),
     install_requires=[
         "sentry-sdk>=2.13.0",
-        "pika>=1.3.2"
+        "pika>=1.3.2",
+        "boto3>=1.34.35",
     ],
     python_requires='>=3.6',
     setup_requires=[
