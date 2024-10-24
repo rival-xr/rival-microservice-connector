360f7d1b0604997ff682c9298ecbf31cf23ea6b6
Nicolas Prevot <nicolas.prevot@rival-xr.com>
2024-10-24 15:06:10 +0100

    also support creds in~/.aws/credentials

--- rival_microservice_connector/awsHelper.py
+++ rival_microservice_connector/awsHelper.py
@@ -8,32 +8,22 @@ def get_session():
     aws_region = os.environ.get('AWS_REGION')
     if not aws_region:
         raise Exception("Error: AWS_REGION environment variable must be set.")
-    access_key, secret_key = get_credentials()
-    return boto3.session.Session(
-            aws_access_key_id=access_key,
-            aws_secret_access_key=secret_key,
-            region_name=aws_region)
-
-def get_credentials():
-    access_key = os.getenv('AWS_ACCESS_KEY_ID')
-    secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+    
     token_file_path = os.getenv('AWS_WEB_IDENTITY_TOKEN_FILE')
-    role_arn = os.getenv('AWS_ROLE_ARN')
-
-    if access_key:
-        if not secret_key:
-            raise Exception("Error: AWS_SECRET_ACCESS_KEY environment variable must be set when using access key.")
-        return access_key, secret_key
-    elif token_file_path:
+    if token_file_path:
         with open(os.getenv("AWS_WEB_IDENTITY_TOKEN_FILE"), 'r') as content_file:
             web_identity_token = content_file.read()
+        role_arn = os.getenv('AWS_ROLE_ARN')
         if not role_arn:
             raise Exception("Error: AWS_ROLE_ARN environment variable must be set when using web identity.")
-
         role = boto3.client('sts').assume_role_with_web_identity(
             RoleArn=os.getenv("AWS_ROLE_ARN"),RoleSessionName='assume-role',
             WebIdentityToken=web_identity_token)
         credentials = role['Credentials']
-        return credentials['AccessKeyId'], credentials['SecretAccessKey']
-    else:
-        raise Exception("Error: Neither AWS_ACCESS_KEY_ID nor AWS_WEB_IDENTITY_TOKEN_FILE is set.")
+        return boto3.session.Session(
+            aws_access_key_id=credentials['AccessKeyId'],
+            aws_secret_access_key=credentials['SecretAccessKey'],
+            aws_session_token=credentials['SessionToken'],
+            region_name=aws_region)
+    # use the default, it might be in via AWS_SECRET_ACCESS_KEY and AWS_ACCESS_KEY_ID
+    return boto3.session.Session(region_name=aws_region)
